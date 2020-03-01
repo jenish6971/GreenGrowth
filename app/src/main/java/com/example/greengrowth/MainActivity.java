61f939441420c53c1bf7cba8de686e5c75051fac
jenish6971 <jenishdhanani6971@gmail.com>
2020-03-01 11:53:32 +0530

    Initial UI

---
+++ app/src/main/java/com/example/greengrowth/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.greengrowth;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    MeowBottomNavigation meo;
+
+    private final static int ID_HOME = 1;
+    private final static int ID_SOILCARD = 2;
+    private final static int ID_SHARE = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar_j = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar_j);
+        toolbar_j.setTitle("Green Growth");
+
+        meo = (MeowBottomNavigation) findViewById(R.id.bottom_nav);
+        meo.add(new MeowBottomNavigation.Model(1, R.drawable.icon_home));
+        meo.add(new MeowBottomNavigation.Model(2, R.drawable.icon_soilcard));
+        meo.add(new MeowBottomNavigation.Model(3, R.drawable.icon_share));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,  new FragmentHome()).commit();
+
+        meo.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                //Toast.makeText(getApplicationContext(), "Clicked item " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        meo.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment select_fragment = null;
+                switch (item.getId()){
+                    case ID_HOME:
+                        select_fragment = new FragmentHome();
+                        break;
+                    case ID_SOILCARD:
+                        select_fragment = new FragmentSoilCard();
+                        break;
+                    case ID_SHARE:
+                        select_fragment = new FragmentShare();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, select_fragment).commit();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.call:
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:7573963878"));
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
